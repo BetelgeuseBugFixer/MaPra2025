c387d814b5d806879a4b20b74c4b2c4ed53b135a
Selin Tuerkoglu <selin.t@live.de>
2025-06-16 17:46:35 +0200

    added architecture for a linear NN

--- models/simple_classifier/simple_classifier.py
+++ models/simple_classifier/simple_classifier.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class ResidueTokenCNN(nn.Module):
-    """1D‐CNN head replacing the MLP."""
+    """1D‐CNN head."""
     def __init__(self, d_emb: int, hidden: int, vocab_size: int, dropout: float = 0.3):
         super().__init__()
         self.conv1 = nn.Conv1d(in_channels=d_emb,
@@ -16,7 +16,7 @@ class ResidueTokenCNN(nn.Module):
                                kernel_size=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        B, L, D = x.shape
+        # B, L, D = x.shape
         x = x.permute(0, 2, 1)       # → (B, d_emb, L)
         h = self.conv1(x)            # → (B, hidden, L)
         h = self.relu(h)
@@ -24,3 +24,25 @@ class ResidueTokenCNN(nn.Module):
         h = self.conv2(h)            # → (B, vocab_size, L)
         out = h.permute(0, 2, 1)     # → (B, L, vocab_size)
         return out
+
+class ResidueTokenLNN(nn.Module):
+    """Linear head."""
+    def __init__(self, d_emb: int, hidden: int, vocab_size: int, dropout: float = 0.3):
+        super().__init__()
+        self.lin1 = nn.Linear(in_features=d_emb,
+                               out_features=hidden)
+        self.relu  = nn.ReLU()
+        self.drop  = nn.Dropout(dropout)
+        self.lin2 = nn.Linear(in_features=hidden,
+                               out_features=vocab_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # B, L, D = x.shape
+        x = x.permute(0, 2, 1)       # → (B, d_emb, L)
+        h = self.lin1(x)            # → (B, hidden, L)
+        h = self.relu(h)
+        h = self.drop(h)
+        h = self.lin2(h)            # → (B, vocab_size, L)
+        out = h.permute(0, 2, 1)     # → (B, L, vocab_size)
+        return out
+
