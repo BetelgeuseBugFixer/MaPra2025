d8b410d1f202c43d4274581a334637e20c6999d3
Aaron Roth <aaron.rot@campus.lmu.de>
2025-06-17 23:03:53 +0200

    working grid search

--- grid_search.sh
+++ grid_search.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#HIDDEN_SIZES=(768 2048 4096 6144)
-#DROPOUTS=(0.2 0.3 0.4)
-#LEARNING_RATES=(0.001 0.0001 0.00001)
-#BATCH_SIZES=(64 128 512 1024)
-#KERNEL_SIZES=(3 5 7 9)
-
-HIDDEN_SIZES=(768 4096)
-DROPOUTS=(0.2)
-LEARNING_RATES=(0.001)
-BATCH_SIZES=(64)
-KERNEL_SIZES=(5)
+HIDDEN_SIZES=(768 2048 4096 6144)
+DROPOUTS=(0.2 0.3 0.4)
+LEARNING_RATES=(0.001 0.0001 0.00001)
+BATCH_SIZES=(128 512 1024 4096)
+KERNEL_SIZES=(3 5 7 9)
 
 mkdir -p tmp_jobs slurm_out grid_search_out
 
@@ -21,14 +15,15 @@ for hs in "${HIDDEN_SIZES[@]}"; do
         for ks in "${KERNEL_SIZES[@]}"; do
 
           job="hs${hs}_dr${dr}_lr${lr}_bs${bs}_ks${ks}"
-          jobfile="tmp_jobs/job_${out_dir}.sbatch"
+          jobfile="tmp_jobs/job_${job}.sbatch"
           out_dir="grid_search_out/${job}"
-          sed -e "s/__HIDDEN_SIZE__/${hs}/" \
-              -e "s/__DROPOUT__/${dr}/" \
-              -e "s/__LEARNING_RATE__/${lr}/" \
-              -e "s/__BATCH_SIZE__/${bs}/" \
-              -e "s/__KERNEL_SIZE__/${ks}/" \
-              -e "s/__OUT_DIR__/${out_dir}/" \
+
+          sed -e "s|__HIDDEN_SIZE__|${hs}|" \
+              -e "s|__DROPOUT__|${dr}|" \
+              -e "s|__LEARNING_RATE__|${lr}|" \
+              -e "s|__BATCH_SIZE__|${bs}|" \
+              -e "s|__KERNEL_SIZE__|${ks}|" \
+              -e "s|__OUT_FOLDER__|${out_dir}|" \
               grid_search_templates/template_grid_search.sbatch > "$jobfile"
 
           sbatch "$jobfile"
