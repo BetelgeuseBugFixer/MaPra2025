4e714caeed41c25a617558310c631b08b174a5f4
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-04 20:59:35 +0200

    added new model for whole pipeline

---
+++ models/whole_model.py
@@ -0,0 +1,55 @@
+from typing import List
+
+import torch
+from torch import nn
+
+from models.foldtoken_decoder.foldtoken_decoder import FoldDecoder
+from models.prot_t5.prot_t5 import ProtT5
+from models.simple_classifier.simple_classifier import ResidueTokenCNN
+
+
+class TFold(nn.Module):
+    def __init__(self, hidden: list, device="cpu", kernel_sizes=[5], dropout: float = 0.3):
+        super().__init__()
+        self.device = device
+        self.plm = ProtT5().to(self.device)
+        embeddings_size = 1024
+        codebook_size = 1024
+        self.cnn = ResidueTokenCNN(embeddings_size, hidden, codebook_size, kernel_sizes, dropout).to(device)
+        self.decoder = FoldDecoder(device=self.device)
+
+        # freeze decoder and plm
+        for param in self.decoder.parameters():
+            param.requires_grad = False
+
+        for param in self.plm.parameters():
+            param.requires_grad = False
+
+    def forward(self, seqs: List[str]):
+        # save the lengths of the sequences
+        true_lengths = [len(seq) for seq in seqs]
+        # prepare seqs
+        x = [" ".join(seq.translate(str.maketrans('UZO', 'XXX'))) for seq in seqs]
+        # generate embeddings
+        x = self.plm(x)
+        # generate tokens
+        x = self.cnn(x)  # shape: (B, L, vocab_size)
+        x = x.argmax(dim=-1)
+        # prepare tokens for decoding
+        vq_codes = []
+        batch_ids = []
+        chain_encodings = []
+        for i, protein_token in enumerate(x):
+            L = true_lengths[i]
+            protein_token_without_padding = protein_token[:L]
+            vq_codes.append(protein_token_without_padding)
+            batch_ids.append(torch.full((L,), i, dtype=torch.long, device=protein_token.device))
+            chain_encodings.append(torch.full((L,), 1, dtype=torch.long, device=protein_token.device))
+        # reshape
+        vq_codes_cat = torch.cat(vq_codes, dim=0)
+        batch_ids_cat = torch.cat(batch_ids, dim=0)
+        chain_encodings_cat = torch.cat(chain_encodings, 0)
+        # decode proteins
+        proteins = self.decoder.decode(vq_codes_cat, chain_encodings_cat, batch_ids_cat)
+        # return proteins and tokens
+        return proteins, x
