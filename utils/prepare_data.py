8fa159d1910f3a540d474ad8f4d44b53572f70f0
Selin Tuerkoglu <selin.t@live.de>
2025-07-09 15:47:08 +0200

    debug 5

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -27,11 +27,10 @@ print("1 Current working directory:", os.getcwd())
 
 # === VAL and TEST ===
 for split in ["val_subset474", "test_subset474"]:
-    pdb_dir = Path(split) / f"{split}_pdb"
+    pdb_dir = Path(f"/mnt/data/large/zip_file/final_data_PDB/{split}") / f"{split}_pdb"
     output_dir = base_output / split
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    print("2 Current working directory:", os.getcwd())
     print(f"Loading {split} from {pdb_dir}")
 
     pdbs = {}
