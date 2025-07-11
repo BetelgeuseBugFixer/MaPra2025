62b98f020a2a6592541318afc7352f7f923c922f
Selin Tuerkoglu <selin.t@live.de>
2025-07-11 18:52:40 +0200

    added bio2token integration

--- models/simple_classifier/simple_classifier.py
+++ models/simple_classifier/simple_classifier.py
@@ -9,8 +9,10 @@ from models.model_utils import _masked_accuracy
 class ResidueTokenCNN(nn.Module):
     """1D‐CNN head."""
 
-    def __init__(self, d_emb: int, hidden: list, vocab_size: int, kernel_sizes=[5], dropout: float = 0.3):
+    def __init__(self, d_emb: int, hidden: list, vocab_size: int, kernel_sizes=[5], dropout: float = 0.1, bio2token: bool = False):
         super().__init__()
+
+        n_atoms_per_residue = 4 if bio2token else 1 # default = 1 for foldtoken. 4 for bio2token
 
         # input conv
         self.conv_in = nn.Conv1d(in_channels=d_emb,
@@ -35,7 +37,7 @@ class ResidueTokenCNN(nn.Module):
 
         # Output projection
         self.conv_out = nn.Conv1d(in_channels=hidden[-1],
-                                  out_channels=vocab_size,
+                                  out_channels=vocab_size * n_atoms_per_residue,     # Codebook size * atoms per residue = new output shape
                                   kernel_size=1)
 
         # save args
@@ -49,7 +51,12 @@ class ResidueTokenCNN(nn.Module):
 
         hidden_layers_string = "_".join(str(i) for i in hidden)
         kernel_sizes_string = "_".join(str(i) for i in kernel_sizes)
-        self.model_name = f"cnn_k{kernel_sizes_string}_h{hidden_layers_string}"
+
+        if bio2token:
+            self.model_name = f"bi_cnn_k{kernel_sizes_string}_h{hidden_layers_string}"
+        else:
+            self.model_name = f"cnn_k{kernel_sizes_string}_h{hidden_layers_string}"
+
         self.criterion = nn.CrossEntropyLoss(ignore_index=PAD_LABEL)
 
         # define most important metric and whether it needs to be minimized or maximized
@@ -117,32 +124,5 @@ class ResidueTokenCNN(nn.Module):
         }
         return score_dict
 
-
-class ResidueTokenLNN(nn.Module):
-    """Linear head."""
-
-    def __init__(self, d_emb: int, hidden: int, vocab_size: int, dropout: float = 0.3):
-        super().__init__()
-        self.lin1 = nn.Linear(in_features=d_emb,
-                              out_features=hidden)
-        self.relu = nn.ReLU()
-        self.drop = nn.Dropout(dropout)
-        self.lin2 = nn.Linear(in_features=hidden,
-                              out_features=vocab_size)
-        self.criterion = nn.CrossEntropyLoss(ignore_index=PAD_LABEL)
-
-        # define most important metric and whether it needs to be minimized or maximized
-        self.key_metric = "val_acc"
-        self.maximize = True
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # B, L, D = x.shape
-        x = x.permute(0, 2, 1)  # → (B, d_emb, L)
-        h = self.lin1(x)  # → (B, hidden, L)
-        h = self.relu(h)
-        h = self.drop(h)
-        h = self.lin2(h)  # → (B, vocab_size, L)
-        out = h.permute(0, 2, 1)  # → (B, L, vocab_size)
-        return out
 
 
