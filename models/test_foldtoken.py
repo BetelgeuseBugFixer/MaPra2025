a6d8857d05bb5b190b91a8c6b5f72a49df5fb2f8
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-12 20:50:02 +0200

    debug prints

--- models/test_foldtoken.py
+++ models/test_foldtoken.py
@@ -441,8 +441,9 @@ if __name__ == '__main__':
         "residue_ids": -1
     }
     batch = [pdb_2_dict(pdb) for pdb in test_pdbs]
+    print(f"2 batch:\n{batch}")
     batch = filter_batch(batch, sequences_to_pad.keys())
-
+    print(f"1 batch:\n{batch}")
     batch = pad_and_stack_batch(
         batch,
         sequences_to_pad,
