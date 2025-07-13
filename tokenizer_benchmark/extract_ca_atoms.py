cd565b047c4205b409dff8169043ac7c7cbadef5
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-13 13:11:20 +0200

    backbone only version of casp14 to test

--- tokenizer_benchmark/extract_ca_atoms.py
+++ tokenizer_benchmark/extract_ca_atoms.py
@@ -1,21 +1,22 @@
 import os
 
 
-def rewrite_pdb(pdb_in_path, pdb_out_path):
+def rewrite_pdb(pdb_in_path, pdb_out_path, allowed_atom_list=["CA"]):
     with open(pdb_in_path, 'r') as in_file:
         with open(pdb_out_path, 'w') as out_file:
             for line in in_file:
                 if line.startswith('ATOM'):
-                    if line[12:16].strip() == "CA":
+                    if line[12:16].strip() in allowed_atom_list:
                         out_file.write(line)
                 else:
                     out_file.write(line)
 
-def main(input_dir="tokenizer_benchmark/casps/casp14", output_dir="tokenizer_benchmark/casps/casp14_edited"):
+
+def main(input_dir="tokenizer_benchmark/casps/casp14", output_dir="tokenizer_benchmark/casps/casp14_backbone"):
     for filename in os.listdir(input_dir):
-        pdb_in_path=os.path.join(input_dir, filename)
-        pdb_out_path=os.path.join(output_dir, filename)
-        rewrite_pdb(pdb_in_path, pdb_out_path)
+        pdb_in_path = os.path.join(input_dir, filename)
+        pdb_out_path = os.path.join(output_dir, filename)
+        rewrite_pdb(pdb_in_path, pdb_out_path,["N", "CA", "C", "O"])
 
 
 if __name__ == '__main__':
