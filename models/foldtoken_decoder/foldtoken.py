3694e38000bf17e365714f5bfe1e67206c98a16a
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-18 22:57:10 +0200

    fixed wird import

--- models/foldtoken_decoder/foldtoken.py
+++ models/foldtoken_decoder/foldtoken.py
@@ -5,7 +5,6 @@ from omegaconf import OmegaConf
 
 from models.foldtoken_decoder.src.data import Protein
 from models.foldtoken_decoder.src.model_interface import MInterface
-from test_data_loader import vq_ids
 
 
 class FoldToken(nn.Module):
@@ -40,23 +39,24 @@ class FoldToken(nn.Module):
     def decode_single_protein_vqs_to_structure(self, vq_codes):
         h_V = self.model.model.vq.embed_id(vq_codes, self.level)
         L, _ = h_V.shape
-        X_t = torch.rand(L,4,3, device='cuda')
+        X_t = torch.rand(L, 4, 3, device='cuda')
         batch_id = torch.zeros(L, device='cuda').long()
         virtual_frame_num = 3
         chain_encoding = torch.ones_like(vq_codes, device=self.device)
-        return self.model.model.decoder_struct.infer_X(X_t, h_V,  batch_id, chain_encoding, 30, virtual_frame_num=virtual_frame_num)
+        return self.model.model.decoder_struct.infer_X(X_t, h_V, batch_id, chain_encoding, 30,
+                                                       virtual_frame_num=virtual_frame_num)
 
-    def decode(self,vq_codes_batch, chain_encodings, batch_ids):
+    def decode(self, vq_codes_batch, chain_encodings, batch_ids):
         # get latent embeddings
         h_V = self.model.model.vq.embed_id(vq_codes_batch, self.level)  # (B, L, D)
         # no fucking idea why this is needed, but it is in the original code
         L, _ = h_V.shape
-        X_t = torch.rand(L,4,3, device='cuda')
+        X_t = torch.rand(L, 4, 3, device='cuda')
         # return get predicted structure
-        X_pred, all_preds = self.model.model.decoder_struct.infer_X(X_t, h_V,  batch_ids, chain_encodings)
+        X_pred, all_preds = self.model.model.decoder_struct.infer_X(X_t, h_V, batch_ids, chain_encodings)
         # create protein objects
         proteins = []
-        for i in range(batch_ids.max()+1):
+        for i in range(batch_ids.max() + 1):
             idx = batch_ids == i
             X_i = X_pred[idx]
             C_i = chain_encodings[idx]
@@ -72,7 +72,7 @@ class FoldToken(nn.Module):
         chain_encoding = torch.ones_like(vq_codes, device=self.device)
         # decode to protein object
         protein = self.model.model.decoding(h_V, chain_encoding)
-        #protein.to(output_path)
+        # protein.to(output_path)
         return protein
 
     def encode_pdb(self, pdb_path):
@@ -81,12 +81,12 @@ class FoldToken(nn.Module):
             vq_code = self.model.encode_protein(protein, level=self.level)[1]
             return vq_code
 
-    def encode_lists_of_pdbs(self, pdb_paths,device):
+    def encode_lists_of_pdbs(self, pdb_paths, device):
         list_of_proteins = []
         for pdb_path in pdb_paths:
             list_of_proteins.append(Protein(pdb_path, device='cuda'))
         with torch.no_grad():
             batch = self.model.batch_proteins(list_of_proteins)
-            vq_ids=self.model.encode_only(batch)
+            vq_ids = self.model.encode_only(batch)
 
         return vq_ids
