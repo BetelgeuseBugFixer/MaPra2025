cfdd1bb3f2c514e548e5553b03364d471f1e1d0c
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-22 23:04:36 +0200

    forgot ()

--- models/end_to_end/whole_model.py
+++ models/end_to_end/whole_model.py
@@ -12,6 +12,127 @@ from models.prot_t5.prot_t5 import ProtT5
 from models.datasets.datasets import PAD_LABEL
 from models.simple_classifier.simple_classifier import ResidueTokenCNN
 
+class FinalFinalModel(nn.Module):
+    def __init__(self, hidden: list, device="cpu", kernel_sizes=[5], dropout: float = 0.1, plm_lora=False,
+                 decoder_lora=False,alpha=1,beta=1):
+        for kernel_size in kernel_sizes:
+            assert kernel_size % 2 == 1, f"Kernel size {kernel_size} is invalid. Must be odd for symmetric context."
+        super().__init__()
+        self.device = device
+        self.plm = ProtT5(use_lora=plm_lora, device=device).to(self.device)
+        embeddings_size = 1024
+        self.decoder = Bio2tokenDecoder(device=device, use_lora=decoder_lora).to(device)
+        codebook_size = 128
+        self.cnn = ResidueTokenCNN(embeddings_size, hidden, codebook_size, kernel_sizes, dropout,
+                                   bio2token=True).to(device)
+
+        self.plm_lora = plm_lora
+
+
+        self.args = {
+            "hidden": hidden,
+            "kernel_sizes": kernel_sizes,
+            "dropout": dropout,
+            "device": device,
+            "plm_lora": plm_lora,
+            "decoder_lora": decoder_lora,
+        }
+        hidden_layers_string = "_".join(str(i) for i in hidden)
+        kernel_sizes_string = "_".join(str(i) for i in kernel_sizes)
+        lora_string = "_plm_lora" if plm_lora else ""
+        self.model_name = f"final_final_k{kernel_sizes_string}_h{hidden_layers_string}{lora_string}"
+
+        # define most important metric and whether it needs to be minimized or maximized
+        self.key_metric = "val_loss"
+        self.maximize = False
+
+    def save(self, output_dir: str, suffix=""):
+        os.makedirs(output_dir, exist_ok=True)
+        save_path = os.path.join(output_dir, f"{self.model_name}{suffix}.pt")
+        torch.save({
+            "model_args": self.args,
+            "state_dict": self.state_dict()
+        }, save_path)
+
+    @staticmethod
+    def load_final_final(path: str, device="cpu") -> "FinalModel":
+        checkpoint = torch.load(path, map_location=device)
+        model_args = checkpoint["model_args"]
+        model_args["device"] = device
+
+        model = FinalFinalModel(**model_args)
+        model.load_state_dict(checkpoint["state_dict"])
+        model.to(device)
+        model.eval()
+        return model
+
+    def forward(self, seqs: List[str]):
+        # save the lengths of the sequences
+        true_lengths = [len(seq) for seq in seqs]
+        # prepare seqs
+        x = [" ".join(seq.translate(str.maketrans('UZO', 'XXX'))) for seq in seqs]
+        # generate embeddings
+        x = self.plm(x)
+        # generate tokens
+        return self.forward_from_embedding(x, true_lengths)
+
+    def forward_from_embedding(self, x, true_lengths=None):
+        # make sure x is on the device
+        x = x.to(device=self.device)
+        if true_lengths is None:
+            # if we do not have lengths derive them from embeddings
+            true_lengths = (x.abs().sum(-1) > 0).sum(dim=1)
+        cnn_out = self.cnn(x)
+        B, L, _ = cnn_out.shape
+        eos_mask = torch.ones(B, L, dtype=torch.bool, device=x.device)
+        for i, length in enumerate(true_lengths):
+            eos_mask[i, :length * 4] = False
+        x = self.decoder.decoder.decoder(cnn_out, eos_mask)
+        # create mask for all relevant positions
+        final_mask = ~eos_mask
+        return x, final_mask, cnn_out
+
+    def run_epoch(self, loader, optimizer=None, device="cpu"):
+        is_train = optimizer is not None
+        self.train() if is_train else self.eval()
+        # init statistics
+        total_loss = total_lddt_loss = total_encoding_loss = total_samples = 0
+
+        torch.set_grad_enabled(is_train)
+        lddt_loss_module = SmoothLDDTLoss().to(device)
+        # if we finetune we expect embeddings, otherwise sequences so we have to adapt the forward Method
+        if self.plm_lora:
+            forward = self.forward
+        else:
+            forward = self.forward_from_embedding
+        for model_in, encoding, structure in loader:
+            #model in is not loaded to device, because it might be a list of sequences
+            encoding, structure = encoding.to(device), structure.to(device)
+            predictions, final_mask, cnn_out = forward(model_in)
+            # get loss:
+            # standard loss
+            B, L, _ = predictions.shape
+            is_dna = torch.zeros((B, L), dtype=torch.bool, device=device)
+            is_rna = torch.zeros((B, L), dtype=torch.bool, device=device)
+            lddt_loss = lddt_loss_module(predictions, structure, is_dna, is_rna, final_mask)
+            if is_train:
+                optimizer.zero_grad()
+                lddt_loss.backward()
+                optimizer.step()
+
+            total_lddt_loss += lddt_loss.detach().item() * B
+            total_samples += B
+            del predictions, final_mask, lddt_loss
+
+        set_prefix = ""
+        if not is_train:
+            set_prefix = "val_"
+        score_dict = {
+            f"{set_prefix}lddt_loss": total_lddt_loss / total_samples,
+        }
+        return score_dict
+
+
 
 class FinalModel(nn.Module):
     def __init__(self, hidden: list, device="cpu", kernel_sizes=[5], dropout: float = 0.1, plm_lora=False,
