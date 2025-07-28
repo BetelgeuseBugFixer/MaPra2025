e7800c25545406a5e172eb6c59d646f3385240e4
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-28 19:15:15 +0200

    more epochs to test

--- models/test_foldtoken.py
+++ models/test_foldtoken.py
@@ -566,7 +566,7 @@ def test_new_model():
     optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
     model.train()
     # run through model:
-    for epoch in range(100):
+    for epoch in range(200):
         optimizer.zero_grad()
         predictions, final_mask, cnn_out = model(seqs)
         #vector_loss = masked_mse_loss(cnn_out, gt_vector, final_mask)
