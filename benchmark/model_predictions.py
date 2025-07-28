6958bfa55aa942acf1c09c53cd756156c48c0450
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-07-28 10:58:42 +0200

    increased batch size for esm

--- benchmark/model_predictions.py
+++ benchmark/model_predictions.py
@@ -277,9 +277,9 @@ if __name__ == '__main__':
 
     # ESMFold
     model = EsmFold(device)
-    compute_and_save_scores_for_model("", model, seqs, pdb_paths, pdb_dicts, batch_size=1, dataset_name="test",given_base="ESMFold")
     try:
-        compute_and_save_scores_for_model("", model, seqs_casp, pdb_casp, casp_dicts, batch_size=1,
+        compute_and_save_scores_for_model("", model, seqs_casp, pdb_casp, casp_dicts, batch_size=4,
                                           dataset_name="casp",given_base="ESMFold")
     except Exception as e:
         print("casp fail")
+    compute_and_save_scores_for_model("", model, seqs, pdb_paths, pdb_dicts, batch_size=4, dataset_name="test",given_base="ESMFold")
