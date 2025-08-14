5acb211f54b0dd5cc41a194a3c4e51f96f03519b
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-08-14 21:13:57 +0200

    debug statements

--- models/train.py
+++ models/train.py
@@ -471,6 +471,9 @@ def _save_reference_pdb(sample, out_dir, tag, atoms_per_res=4):
     else:
         gt_struct=gt_struct.unsqueeze(0)
         B, L,_ = gt_struct.shape
+        print(f"B:{B}")
+        print(f"L:{L}")
+        print(model_in)
         final_mask = torch.zeros(B, L * atoms_per_res, dtype=torch.bool)
         for i in range(len(model_in)):
             final_mask[i, :len(model_in[i]) * atoms_per_res] = True
