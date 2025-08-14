580ef0fd08c7c428f51a615b7267e325dbab8e04
Aaron Roth <rotha@cip.ifi.lmu.de>
2025-08-14 17:55:36 +0200

    fixed typo

--- models/test_foldtoken.py
+++ models/test_foldtoken.py
@@ -873,7 +873,7 @@ def write_pdb():
             smoooth_lddt = 1 - orig_value
             # calc other scores
             gt_protein = load_prot_from_pdb(pdb_path)
-            pred_protein = model_prediction_to_atom_array(seqs,pred_protein,final_mask)
+            pred_protein = model_prediction_to_atom_array(seqs,backbone_coords,final_mask)
             normal_lddt = float(lddt(gt_protein, pred_protein))
 
             # append score to list
